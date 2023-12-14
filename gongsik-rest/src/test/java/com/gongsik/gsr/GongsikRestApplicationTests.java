09c706a861d2348c43c805531a556a83723629dc
pc <djop1212@gmail.com>
2023-12-14 10:34:46 +0900

    2023.12.14 <restAPI서버 생성>

---
+++ gongsik-rest/src/test/java/com/gongsik/gsr/GongsikRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongsik.gsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongsikRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
