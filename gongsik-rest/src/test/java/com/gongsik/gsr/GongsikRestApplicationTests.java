820c825e556158760fd507244d2647d4d064dee0
sdchoi <djop1212@gmail.com>
2023-12-20 17:17:28 +0900

    2023.12.20 <로그 재 설정>

--- gongsik-rest/src/test/java/com/gongsik/gsr/GongsikRestApplicationTests.java
+++ gongsik-rest/src/test/java/com/gongsik/gsr/GongsikRestApplicationTests.java
@@ -1,22 +1,36 @@
 package com.gongsik.gsr;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GongsikRestApplicationTests {
 	
 	@Autowired
-	private MockMvc mvc;
+	private MockMvc mockMvc;
 	@Test
 	void contextLoads() throws Exception {
+			menuListSelect();
 		
-			mvc.perform(get("/main")).andDo(print());
+		
+	
+	
+	}
+	
+	//메뉴 조회 리스트 
+	void menuListSelect() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/main/menuList") // API 엔드포인트 URL
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
 	}
 
 }
